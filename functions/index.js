4157a8233b2ea2556856b7f6c502e23018bf1e13
Miller Adulu <milleradulu@gmail.com>
2019-04-13 03:18:27 +0300

    Add data bed data to records

--- functions/index.js
+++ functions/index.js
@@ -4,12 +4,18 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 exports.addAlert = functions.https.onRequest((req, res) => {
-    return admin.firestore().collection('alerts').add({
-        humidity: req.body.humidity,
-        temp: req.body.temp,
-        time: new Date(),
-        sensorId: req.body.sensorId
-    }).then(ref => {
-        return res.json({ result: `Added document with ID: ${ref.id}` });
+    return admin.firestore().collection('sensors').doc(req.body.sensorId).get().then(sensorRecord => {
+        const sensor = sensorRecord.data()
+
+        return admin.firestore().collection('alerts').add({
+            humidity: req.body.humidity,
+            temp: req.body.temp,
+            time: new Date(),
+            sensorId: req.body.sensorId,
+            bed: sensor.bedNo
+        }).then(ref => {
+            return res.json({ result: `Added document with ID: ${ref.id}` });
+        });
     });
-})
+
+});
