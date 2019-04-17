c45f56a8e55392905dc7fbc0aeec2eb08811885b
Miller Adulu <milleradulu@gmail.com>
2019-04-17 15:14:46 +0300

    Add gas parameter to data

--- functions/index.js
+++ functions/index.js
@@ -4,19 +4,24 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 exports.addAlert = functions.https.onRequest((req, res) => {
-    return admin.firestore().collection('sensors').doc(req.body.sensorId).get().then(sensorRecord => {
-        const sensor = sensorRecord.data()
+    temp = req.body.temp
+    humidity = req.body.humidity
+    gas = req.body.gas
 
-        return admin.firestore().collection('alerts').add({
-            humidity: req.body.humidity,
-            temp: req.body.temp,
-            sensorId: req.body.sensorId,
-            bed: sensor.bedNo,
-            handled: false,
-            time: new Date(),
-        }).then(ref => {
-            return res.json({ result: `Added document with ID: ${ref.id}` });
+        return admin.firestore().collection('sensors').doc(req.body.sensorId).get().then(sensorRecord => {
+            const sensor = sensorRecord.data()
+
+            return admin.firestore().collection('alerts').add({
+                humidity,
+                temp,
+                gas,
+                sensorId: req.body.sensorId,
+                bed: sensor.bedNo,
+                handled: false,
+                time: new Date(),
+            }).then(ref => {
+                return res.json({ result: `Added document with ID: ${ref.id}` });
+            });
         });
-    });
 
 });
